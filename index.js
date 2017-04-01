2af2b2f4ad3b3d16cbc81110c11d022108bcb339
Sasha <sasha.svyatets@gmail.com>
2017-04-01 20:08:41 +0300

    Change to NotifySend (Ubuntu) only usage. Add icon.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const notifier = require('node-notifier');
+const NotifySend = require('node-notifier').NotifySend;
+var notifier = new NotifySend();
 var CronJob = require('cron').CronJob;
 var id = 0;
 
-new CronJob('00 */15 * * * *', function() {
+new CronJob('* * * * * *', function() {
   id++;
   var now = new Date();
   var dateStr = now.getDate() + "." + ( now.getMonth() + 1 ) + ".";
@@ -10,6 +11,9 @@ new CronJob('00 */15 * * * *', function() {
   console.log(id + ".\t|" + dateStr + '| You will see this message every 15 minutes.');
   notifier.notify({
     'title': 'I love myself! I care about my health!',
-    'message': '15 minutes passed. I take a break!'
+    'message': '15 minutes passed. I take a break!',
+    //'icon': __dirname + '/coulson.jpg',
+    //Uses logo of atom text editor (require atom installed)
+    'icon': '/usr/share/icons/hicolor/128x128/apps/atom.png'
   });
 }, null, true, 'Europe/Kiev');
